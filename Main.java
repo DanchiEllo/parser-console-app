2c97da80c0ca5e5ed4ac9355c4f303350c48fa75
DanchiEllo <94111499+DanchiEllo@users.noreply.github.com>
2023-07-14 06:47:21 +0300

    Create Main.java
    Main class

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.simbirsoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Log4jConfig.configure();
+        ConsoleIntarface.startMenu();
+        
+    }
+
+}
